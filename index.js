e341a1d9814afcf9fcbb4334536bdca9d14bd405
RobertSGomes <robertsg.dev@gmail.com>
2022-08-18 19:33:40 -0300

    Finalizado

---
+++ index.js
@@ -0,0 +1,34 @@
+document.getElementById("numbers").addEventListener("keypress", function (e) {
+    if (e.key == "Enter") {
+        criarInputs(e.target.value)
+    }
+})
+
+function criarInputs(qtdes) {
+    document.getElementById("valores").innerHTML = ""
+
+    for (let i = 0; i < qtdes; i++) {
+        document.getElementById("valores").innerHTML += `
+            <input type="number" id="inp_${i}">
+        `
+    }
+
+    document.getElementById("valores").innerHTML += `
+        <button type="button" id="calcular">CALCULAR MÉDIA</button>
+    `
+
+    document.getElementById("calcular").addEventListener("click", function () {
+        calcularMedia()
+    })
+}
+
+function calcularMedia() {
+    const numeros = document.getElementById("numbers").value
+    var total = 0
+
+    for (let i = 0; i < numeros; i++) {
+        total += parseInt(document.getElementById(`inp_${i}`).value)
+    }
+
+    alert("média: " + total / numeros)
+}
